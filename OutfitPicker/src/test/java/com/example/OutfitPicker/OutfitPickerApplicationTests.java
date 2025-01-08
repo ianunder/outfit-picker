9fe9665522302427848cff8162dee35de76ec409
ianunder <ianunderwood470@gmail.com>
2025-01-07 20:14:34 -0500

    Initial project structure

---
+++ OutfitPicker/src/test/java/com/example/OutfitPicker/OutfitPickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OutfitPicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutfitPickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
